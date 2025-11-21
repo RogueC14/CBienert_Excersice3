bab9079dc4dea99f9fde2ed5f113267500ff5bff
Candace <c.bienert@uleth.ca>
2025-11-21 13:45:17 -0700

    DONE with first session

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Connected, as instructed. But now more nonesense because no one is going to see it. ^-^")
+console.log("__--__--__--_i_i")
+
+$(document.body).css("background", "#dbdae2")
+$(".title h1").html("Abundant Memory, <em>Relational Intelligences</em>")
+$(".title").append("<p>October 17th 2025 - January 23, 2025</p>")
+$(".titles").removeClass("type");
+$(".title").css("font-family", "Impact")
+$(".funding").html("<p>This exhibition draws upon research supported by the Government of Canada’s New Frontiers in Research Fund (NFRF) and the Social Sciences and Humanities Research Council, and the Canada Foundation for Innovation.</p>")
+$(".logos").html("<img src='assets/ulag_logo_black.webp'>")
